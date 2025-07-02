b5930d179e595c500bfccc1d736ad1726e17e0f4
adevense <adevense016@gmail.com>
2025-07-01 22:43:30 -0400

    b

--- ex1.py
+++ ex1.py
@@ -2,10 +2,10 @@ admministrativo =['alison','jhonas','lucas']
 operacional =['alison','jhonas','lucas']
 tecnico =['alison','jhonas','lucas']
 
-tudo =admministrativo, tecnico
-print(tudo)
-"""
+
+
+
 def sorteio(**keyargs):
+    
     print(keyargs)
-sorteio( admministrativo, operacional)
-"""
+sorteio(admministrativo, operacional, tecnico)
